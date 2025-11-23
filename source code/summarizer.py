55d26b15d096bc652d18745211b0ec7b32a6a9d6
aarushi25bai10691 <aarushi.25bai10691@vitbhopal.ac.in>
2025-11-23 13:05:18 +0530

    Delete source code directory

--- source code/summarizer.py
+++
@@ -1,9 +0,0 @@
-# summarizer.py
-def show_summary(df):
-    if df.empty:
-        print("No expense data found.")
-        return
-    summary = df.groupby('Category')['Amount'].sum()
-    print("Expense Summary by Category:")
-    for cat, amt in summary.items():
-        print(f"{cat}: {amt:.2f}")
--- source code/utils.py
+++
@@ -1,7 +0,0 @@
-# utils.py
-def validate_category(category):
-    valid_categories = ['Food', 'Rent', 'Entertainment', 'Utilities', 'Transport', 'Other']
-    if category not in valid_categories:
-        print("Invalid category! Choose from:", valid_categories)
-        return False
-    return True
